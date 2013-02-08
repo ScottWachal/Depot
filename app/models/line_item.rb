a1963e464a964b62cf285a6194e38916810c2f31
Scott Wachal <scott.wachal@gmail.com>
2013-02-08 14:57:48 -0600

    Finished quanity issue with cart, modified display and routing

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,7 @@ class LineItem < ActiveRecord::Base
   attr_accessible  :product_id, :quantity, :order_id, :product, :cart, :cart_id 
   belongs_to :cart
   belongs_to :product
+  def total_price
+    quantity * product.price
+  end
 end
