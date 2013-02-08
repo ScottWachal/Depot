47fadbf4438b97f7e5eea1eaeec3acb6d59e53a5
Scott Wachal <scott.wachal@gmail.com>
2013-02-08 11:40:43 -0600

    Added initial cart functionality and tests

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
+  attr_accessible  :product_id, :quantity, :order_id, :product, :cart, :cart_id 
   belongs_to :cart
   belongs_to :product
 end
